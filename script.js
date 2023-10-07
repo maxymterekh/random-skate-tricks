d65ec6e9e04be64147c390b7043bafb54910220a
maxymterekh <109846960+maxymterekh@users.noreply.github.com>
2023-10-07 13:53:53 +0300

    updateeeeeeeek

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const stances = ["regular", "fakie", "switch", "nollie"];
 const tricks = ["kickflip", "heelflip", " bs shove it", " fs shove it", "fs 180",
 "bs 180", "varial flip", "casper flip", "ollie", "bs bigspin ", "360 shove it",
 "360 flip", "varial heel ", "hardflip", "inward heel", "lazer flip", "casper heel ", "bs 180 kickflip",
-"fs 180 kickflip", "bs 180 heelflip", "fs 180 heelflip", "impossible"];
+"fs 180 kickflip", "bs 180 heelflip", "fs 180 heelflip", "impossible", "frontside bigspin", "ollie north", 
+"ollie south", "bs revert", "fs revert", "fs 180 no comply", "backside 360", "frontside 360",
+"ghetto bird", " frontfoot impossible", "dolphin flip", "alpha flip", "bigflip" ];
  
 const stance = document.getElementById("stance");
 const trick = document.getElementById("trick");
